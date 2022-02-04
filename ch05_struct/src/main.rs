609a52633465f895ac0bee60b5825de89f208587
mercuriver <mercuriver@gmail.com>
2022-02-05 01:08:51 +0900

    feat(ch05): 신규 기능 메서드 추가

--- ch05_struct/src/main.rs
+++ ch05_struct/src/main.rs
@@ -5,18 +5,22 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }    
+    // fn area(&self) -> u32 {
+    //     self.width * self.height
+    // }   
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
     let rect1 = Rectangle { width:30, height: 50 };
+    let rect2 = Rectangle { width:10, height: 40 };
+    let rect3 = Rectangle { width:60, height: 45 };
     
-    println!(
-        "사각형의 면적: {} 제곱 픽셀",
-        rect1.area()
-    );
+    // println!("rect1: {:?}", rect1);
+    // println!("사각형의 면적: {} 제곱 픽셀",rect1.area());
 
-    println!("rect1: {:?}", rect1);
+    println!("rect1은 rect2를 포함하는가? {}",rect1.can_hold(&rect2));
+    println!("rect1은 rect3을 포함하는가? {}",rect1.can_hold(&rect3));
 }
