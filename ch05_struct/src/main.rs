8e3532d8bb064c063fb4aa72fb1de5096258e839
mercuriver <mercuriver@gmail.com>
2022-02-04 23:39:08 +0900

    refactor(ch05): 튜플을 이용한 리팩토링, 매개변수 표현력 강화

--- ch05_struct/src/main.rs
+++ ch05_struct/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = (30, 50);
+    
     println!(
         "사각형의 면적: {} 제곱 픽셀",
-        area(width1, height1)
+        area(rect1)
     );
 }
 
-fn area(width:u32, height: u32) -> u32 {
-    width * height
+fn area( dimensions:(u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
