1d17dbcef8fc8e8c73a29eec486b80696b5307a3
mercuriver <mercuriver@gmail.com>
2022-02-06 16:56:42 +0900

    feat(ch07): 구조체, 열거자 예제

--- ch07/src/lib.rs
+++ ch07/src/lib.rs
@@ -7,6 +7,8 @@ mod tests {
     }
 }
 
+fn serve_order() {}
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -20,20 +22,46 @@ mod front_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    // 절대 경로
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // 상대 경로
-    front_of_house::hosting::add_to_waitlist();
-}
-
-fn serve_order() {}
-
 mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("복숭아"),
+            }
+        }
+    }
+
     fn cook_order() {}
     fn fix_incorrect_order() {
         cook_order();
         super::serve_order();
     }
+}
+
+pub fn eat_at_restaurant() {
+    // 절대 경로
+    crate::front_of_house::hosting::add_to_waitlist();
+    // // 상대 경로
+    // front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("플랫브레드");
+    meal.toast = String::from("화이트");
+    println!("{} 토스트로 주세요", meal.toast);
+
+    // // 에러 발생, 접근 불가능
+    // meal.seasonal_fruit = String::from("블루베리");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
