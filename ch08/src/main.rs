cf723de30be5b01bbc5bcae1a1e5ddac3de38dc6
mercuriver <mercuriver@gmail.com>
2022-02-10 20:11:30 +0900

    feat(ch08): 연습문제 #3 문제

---
+++ ch08/src/main.rs
@@ -0,0 +1,13 @@
+/*
+ * 해쉬맵과 벡터를 이용하여, 사용자가 회사 내의 부서에 대한 피고용인 이름을 
+ * 추가할 수 있도록 하는 텍스트 인터페이스를 만들어보세요. 
+ * 예를들어 “Add Sally to Engineering”이나 “Add Amir to Sales” 같은 식으로요. 
+ * 그후 사용자가 각 부서의 모든 사람들에 대한 리스트나 알파벳 순으로 정렬된 
+ * 부서별 모든 사람에 대한 리스트를 조회할 수 있도록 해보세요.
+*/
+use std::io;
+
+fn main() {
+  // Command: Add, Remove, Edit, Search:Name|Department, Order:Asc|Desc, Print, Exit
+
+}
