9765f375b3f2cad06e4b091bd7b3a144be27e662
mercuriver <mercuriver@gmail.com>
2022-02-15 19:58:47 +0900

    feat(ch08): 예제 3, HashMap에 데이터 저장

--- ch08/src/main.rs
+++ ch08/src/main.rs
@@ -6,12 +6,29 @@
  * 부서별 모든 사람에 대한 리스트를 조회할 수 있도록 해보세요.
 */
 use std::io;
+use std::collections::HashMap;
 
 fn trim_input(value: &String) -> &str {
   value.trim()
 }
 
-fn parse_command(command: &str) {
+fn print_type_of<T>(_: &T) {
+  println!("{}", std::any::type_name::<T>());
+}
+
+// #[derive(Debug)]
+// struct Employee {
+//   name: String,
+//   depertment: String,
+// }
+
+// impl Employee {
+//   fn new(name: &str, depertment: &str) -> Employee {
+//     Employee { name: name.to_string(), depertment: depertment.to_string() }
+//   }
+// }
+
+fn parse_command(command: &str, data: &mut HashMap<String, String>) {
   match command {
     "ADD" => {
       println!("사원 정보 입력");
@@ -22,7 +39,18 @@ fn parse_command(command: &str) {
         .expect("유효하지 않은 입력입니다.");
 
       let keywords: Vec<&str> = trim_input(&detail).split(' ').collect();
-      println!("{:?}", keywords);
+      
+      // println!("{:?}", keywords);
+      // print_type_of(&keywords);
+      // print_type_of(&keywords[0]);
+      // println!("{}", keywords[0]);
+
+      // &str(문자열 슬라이스) 보다 문자열로 다루면 되기에 형 변환
+      // keywords로 접근할 경우 &detail의 라이프타임 문제가 발생
+      let name: String = keywords[0].to_string();
+      let depertment: String = keywords[2].to_string();
+
+      data.insert(name, depertment);
     },
     "REMOVE" => println!("사원 삭제 절차 진행"),
     "EDIT" => println!("사원 삭제 절차 진행"),
@@ -34,6 +62,9 @@ fn parse_command(command: &str) {
 }
 
 fn main() {
+  let mut data: HashMap<String, String> = HashMap::new();
+  // let mut data = HashMap::new();
+
   loop {
     println!("작업 형식 입력");
     println!("[Add, Remove, Edit, Search, Order, Print, Exit]");
@@ -53,7 +84,9 @@ fn main() {
           println!("프로그램 종료");
           break;
       }
-      _ => parse_command(&command),
+      _ => parse_command(&command, &mut data),
     }
+
+    println!("\n\n{:?}", data);
   }
 }
