8b207dcc7871db04c73217a1e9833923446ffe54
mercuriver <mercuriver@gmail.com>
2022-04-03 17:55:16 +0900

    refactor(ch12): main 함수 관심사 분리

--- ch12/src/main.rs
+++ ch12/src/main.rs
@@ -1,11 +1,26 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("검색어: {}", config.query);
+    println!("대상 파일: {}", config.filename);
 
-    println!("검색어: {}", query);
-    println!("대상 파일: {}", filename);
+    let contents = fs::read_to_string(config.filename).expect("파일을 읽지 못했습니다.");
+
+    println!("파일 내용: \n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
