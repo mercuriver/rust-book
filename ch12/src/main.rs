b24648f4b3da50be1e9fe7be770870ca7e3c33b1
mercuriver <mercuriver@gmail.com>
2022-04-03 23:09:44 +0900

    refactor(ch12): Config 연관 함수 처리

--- ch12/src/main.rs
+++ ch12/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("검색어: {}", config.query);
     println!("대상 파일: {}", config.filename);
@@ -18,9 +18,10 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
