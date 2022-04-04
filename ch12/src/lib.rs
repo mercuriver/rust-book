2c4ea4716ecfb2fe150cdd7aeb8ae6ff1d0f6352
mercuriver <mercuriver@gmail.com>
2022-04-04 17:30:11 +0900

    test(ch12): lib 테스트 케이스 작성

--- ch12/src/lib.rs
+++ ch12/src/lib.rs
@@ -25,10 +25,29 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   Ok(())
 }
 
-// impl Config {
-//   fn new(args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let filename = args[2].clone();
-//     Config { query, filename }
-//   }
-// }
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let mut results = Vec::new();
+  for line in contents.lines() {
+    if line.contains(query) {
+      results.push(line);
+    }
+  }
+
+  results
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+  }
+}
