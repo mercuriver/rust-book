d27d5aec066544127d51bdc85b987a05cbe09e62
mercuriver <mercuriver@gmail.com>
2022-04-04 16:56:26 +0900

    refactor(ch12): lib 크레이트 분리

---
+++ ch12/src/lib.rs
@@ -0,0 +1,34 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+  pub query: String,
+  pub filename: String,
+}
+
+impl Config {
+  pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    if args.len() < 3 {
+      return Err("필요한 인수가 지정되지 않았습니다.");
+    }
+
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Ok(Config { query, filename })
+  }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+  let contents = fs::read_to_string(config.filename)?;
+  println!("파일 내용: \n{}", contents);
+
+  Ok(())
+}
+
+// impl Config {
+//   fn new(args: &[String]) -> Config {
+//     let query = args[1].clone();
+//     let filename = args[2].clone();
+//     Config { query, filename }
+//   }
+// }
