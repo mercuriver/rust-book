b79725e4c483517d709808bdcd27b14f59207f1b
mercuriver <mercuriver@gmail.com>
2022-04-05 18:39:07 +0900

    feat(ch12): error 채널 분리

--- ch12/src/main.rs
+++ ch12/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
   let args: Vec<String> = env::args().collect();
 
   let config = Config::new(&args).unwrap_or_else(|err| {
-    println!("인수를 구문분석하는 동안 오류가 발생했습니다: {}", err);
+    eprintln!("인수를 구문분석하는 동안 오류가 발생했습니다: {}", err);
     process::exit(1);
   });
 
   if let Err(e) = ch12::run(config) {
-    println!("애플리케이션 에러: {}", e);
+    eprintln!("애플리케이션 에러: {}", e);
     process::exit(1);
   }
 }
