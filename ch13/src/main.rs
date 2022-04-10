c14f1085d9a2eaecd5ac37d3acbd137530df9b85
mercuriver <mercuriver@gmail.com>
2022-04-10 21:57:34 +0900

    feat(ch13): closure 값 캐싱 처리 (Fn trait)

--- ch13/src/main.rs
+++ ch13/src/main.rs
@@ -1,6 +1,36 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T>
+where
+  T: Fn(u32) -> u32,
+{
+  calculation: T,
+  value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+  T: Fn(u32) -> u32,
+{
+  fn new(calculation: T) -> Cacher<T> {
+    Cacher {
+      calculation,
+      value: None,
+    }
+  }
+  fn value(&mut self, arg: u32) -> u32 {
+    match self.value {
+      Some(v) => v,
+      None => {
+        let v = (self.calculation)(arg);
+        self.value = Some(v);
+        v
+      }
+    }
+  }
+}
+
 fn simulated_expensive_caculation(intensity: u32) -> u32 {
   println!("시간이 오래 걸리는 작업.");
   thread::sleep(Duration::from_secs(2));
@@ -8,20 +38,20 @@ fn simulated_expensive_caculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_numbr: u32) {
-  let expensive_closure = |num: u32| -> u32 {
+  let mut expensive_result = Cacher::new(|num: u32| -> u32 {
     println!("시간이 오래 걸리는 작업.");
     thread::sleep(Duration::from_secs(2));
     num
-  };
+  });
 
   if intensity < 25 {
-    println!("팔 굽혀펴기 {}번", expensive_closure(intensity));
-    println!("윗몸 일으키기 {}번", expensive_closure(intensity));
+    println!("팔 굽혀펴기 {}번", expensive_result.value(intensity));
+    println!("윗몸 일으키기 {}번", expensive_result.value(intensity));
   } else {
     if random_numbr == 3 {
       println!("수분을 충분히 섭취하세요.");
     } else {
-      println!("달리기 {}분", expensive_closure(intensity));
+      println!("달리기 {}분", expensive_result.value(intensity));
     }
   }
 }
