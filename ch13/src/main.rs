8169092a9d87a8a3beae81c6d93cf46c08866848
mercuriver <mercuriver@gmail.com>
2022-04-10 21:49:04 +0900

    refactor(ch13): closure 적용

--- ch13/src/main.rs
+++ ch13/src/main.rs
@@ -8,16 +8,20 @@ fn simulated_expensive_caculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_numbr: u32) {
-  let expensive_result = simulated_expensive_caculation(intensity);
+  let expensive_closure = |num: u32| -> u32 {
+    println!("시간이 오래 걸리는 작업.");
+    thread::sleep(Duration::from_secs(2));
+    num
+  };
 
   if intensity < 25 {
-    println!("팔 굽혀펴기 {}번", expensive_result);
-    println!("윗몸 일으키기 {}번", expensive_result);
+    println!("팔 굽혀펴기 {}번", expensive_closure(intensity));
+    println!("윗몸 일으키기 {}번", expensive_closure(intensity));
   } else {
     if random_numbr == 3 {
       println!("수분을 충분히 섭취하세요.");
     } else {
-      println!("달리기 {}분", expensive_result);
+      println!("달리기 {}분", expensive_closure(intensity));
     }
   }
 }
