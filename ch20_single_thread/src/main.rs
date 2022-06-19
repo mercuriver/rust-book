d9cc2c0ebf45ef1da7a9652ae70c9ea38b64f8c9
mercuriver <mercuriver@gmail.com>
2022-06-19 20:11:01 +0900

    feat(ch20): serve html file

--- ch20_single_thread/src/main.rs
+++ ch20_single_thread/src/main.rs
@@ -1,10 +1,10 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
         handle_connection(_stream);
@@ -12,13 +12,11 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-
-    println!("요청: {}", String::from_utf8_lossy(&buffer[..]));
 }
