b43fb784093545d26c63f4df48d51365d2bd95b5
mercuriver <mercuriver@gmail.com>
2022-06-19 19:49:24 +0900

    feat(ch20): handle tcp stream buffer

--- ch20_single_thread/src/main.rs
+++ ch20_single_thread/src/main.rs
@@ -1,10 +1,18 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        println!("연결 됨");
+        let _stream = stream.unwrap();
+        handle_connection(_stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("요청: {}", String::from_utf8_lossy(&buffer[..]));
 }
