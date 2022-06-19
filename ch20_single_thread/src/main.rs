2c39ae9887e4b86c514127e364d706a1759304b5
mercuriver <mercuriver@gmail.com>
2022-06-19 19:54:28 +0900

    feat(ch20): return tcp response

--- ch20_single_thread/src/main.rs
+++ ch20_single_thread/src/main.rs
@@ -14,5 +14,11 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
     println!("요청: {}", String::from_utf8_lossy(&buffer[..]));
 }
