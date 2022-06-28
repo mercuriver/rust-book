bd66d21b068d881911378df3bc176971383b7a0d
mercuriver <mercuriver@gmail.com>
2022-06-28 14:08:23 +0900

    feat(ch20): multi thread 라이브러리 크레이트로 전환 및 프로토타입 정의

--- ch20_multi_thread/src/bin/main.rs
+++ ch20_multi_thread/src/bin/main.rs
@@ -5,13 +5,16 @@ use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
 
+use ch20_multi_thread::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(_stream);
         });
     }
