4580b0ce8811f62e627eec86c1d4439d82c6d09a
mercuriver <mercuriver@gmail.com>
2022-06-28 14:24:38 +0900

    feat(20): doc 문서 작성

--- ch20_multi_thread/src/lib.rs
+++ ch20_multi_thread/src/lib.rs
@@ -1,6 +1,13 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
+  /// 새 ThreadPool 인스턴스 생성
+  ///  
+  /// size 매개변수로 Thread pool size 지정
+  ///
+  /// # Panics
+  ///
+  /// size 매개변수의 값은 '0' 이상이여야 함
   pub fn new(size: usize) -> ThreadPool {
     ThreadPool
   }
