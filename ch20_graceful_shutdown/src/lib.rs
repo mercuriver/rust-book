e8d85897135993fdf7318e248f5d26f1e7d45364
mercuriver <mercuriver@gmail.com>
2022-06-28 18:22:28 +0900

    fix(ch20): Terminate 처리 단일 대상으로 발생하던 오류 수정

--- ch20_graceful_shutdown/src/lib.rs
+++ ch20_graceful_shutdown/src/lib.rs
@@ -27,6 +27,7 @@ impl Worker {
         }
         Message::Terminate => {
           println!("종료 메시지 수선: 작업자 {}", id);
+          break;
         }
       }
     });
@@ -60,7 +61,7 @@ impl ThreadPool {
     let mut workers = Vec::with_capacity(size);
 
     for id in 0..size {
-      workers.push(Worker::new(id, Arc::clone(&receiver)))
+      workers.push(Worker::new(id, Arc::clone(&receiver)));
     }
 
     ThreadPool { workers, sender }
